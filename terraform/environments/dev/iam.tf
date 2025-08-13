f53f72c9dfc9f07982fb123bfc794ab026effe5c
hassan <hassan@gmail.com>
2025-08-13 08:56:04 +0530

    updated

--- terraform/environments/dev/iam.tf
+++ terraform/environments/dev/iam.tf
@@ -1,3 +1,6 @@
+# ---------------------------
+# EKS Cluster IAM Role
+# ---------------------------
 resource "aws_iam_role" "eks_cluster_role" {
   name = "eksClusterRole"
 
@@ -13,6 +16,7 @@ resource "aws_iam_role" "eks_cluster_role" {
   })
 }
 
+# Attach required policies 
 resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
   role       = aws_iam_role.eks_cluster_role.name
@@ -23,7 +27,14 @@ resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSVPCResourceContr
   role       = aws_iam_role.eks_cluster_role.name
 }
 
+resource "aws_iam_role_policy_attachment" "eks_cluster_AdminAccess" {
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+  role       = aws_iam_role.eks_cluster_role.name
+}
 
+# ---------------------------
+# Worker Node IAM Role
+# ---------------------------
 resource "aws_iam_role" "worker_node_role" {
   name = "eksWorkerNodeRole"
 
@@ -39,6 +50,7 @@ resource "aws_iam_role" "worker_node_role" {
   })
 }
 
+# Attach required policies 
 resource "aws_iam_role_policy_attachment" "worker_node_AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
   role       = aws_iam_role.worker_node_role.name
@@ -51,5 +63,10 @@ resource "aws_iam_role_policy_attachment" "worker_node_AmazonEC2ContainerRegistr
 
 resource "aws_iam_role_policy_attachment" "worker_node_AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.worker_node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "worker_node_AdminAccess" {
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
   role       = aws_iam_role.worker_node_role.name
 }
