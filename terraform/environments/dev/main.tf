6d5d816171d43fe6ebdd88b0052fd050233e53e4
hassan <hassan@gmail.com>
2025-08-13 08:37:19 +0530

    updated

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -50,16 +50,79 @@ module "ec2_instances" {
 # IAM ROLES (EKS) #
 ###################
 
-data "aws_iam_role" "eks_cluster_role" {
-  count = var.enable_eks ? 1 : 0
-  name  = "eksClusterRole"
+# ---------------------------
+# EKS Cluster IAM Role
+# ---------------------------
+resource "aws_iam_role" "eks_cluster_role" {
+  name = "eksClusterRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "eks.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
 }
 
-# Worker node role (you must create this IAM role in AWS with proper policies)
-data "aws_iam_role" "worker_node_role" {
-  count = var.enable_eks ? 1 : 0
-  name  = "eksWorkerNodeRole"
+# Attach required policies 
+resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSClusterPolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
+  role       = aws_iam_role.eks_cluster_role.name
 }
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_AmazonEKSVPCResourceController" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
+  role       = aws_iam_role.eks_cluster_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "eks_cluster_AdminAccess" {
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+  role       = aws_iam_role.eks_cluster_role.name
+}
+
+# ---------------------------
+# Worker Node IAM Role
+# ---------------------------
+resource "aws_iam_role" "worker_node_role" {
+  name = "eksWorkerNodeRole"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Effect = "Allow"
+      Principal = {
+        Service = "ec2.amazonaws.com"
+      }
+      Action = "sts:AssumeRole"
+    }]
+  })
+}
+
+# Attach required policies 
+resource "aws_iam_role_policy_attachment" "worker_node_AmazonEKSWorkerNodePolicy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+  role       = aws_iam_role.worker_node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "worker_node_AmazonEC2ContainerRegistryReadOnly" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
+  role       = aws_iam_role.worker_node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "worker_node_AmazonEKS_CNI_Policy" {
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
+  role       = aws_iam_role.worker_node_role.name
+}
+
+resource "aws_iam_role_policy_attachment" "worker_node_AdminAccess" {
+  policy_arn = "arn:aws:iam::aws:policy/AdministratorAccess"
+  role       = aws_iam_role.worker_node_role.name
+}
+
 
 ##################
 # EKS CLUSTER    #
@@ -68,15 +131,13 @@ data "aws_iam_role" "worker_node_role" {
 module "eks" {
   source               = "../../modules/eks"
   cluster_name         = "my-ec2-eks"
-  cluster_role_arn     = data.aws_iam_role.eks_cluster_role[0].arn
-  worker_node_role_arn = data.aws_iam_role.worker_node_role[0].arn
-  # cluster_role_arn     = aws_iam_role.eks_cluster_role.arn
-  # worker_node_role_arn = aws_iam_role.worker_node_role.arn
-  subnet_ids        = module.network.private_subnet_ids
-  instance_types    = ["t3.small"]
-  node_desired_size = 2
-  node_min_size     = 1
-  node_max_size     = 3
+  cluster_role_arn     = aws_iam_role.eks_cluster_role.arn
+  worker_node_role_arn = aws_iam_role.worker_node_role.arn
+  subnet_ids           = module.network.private_subnet_ids
+  instance_types       = ["t3.small"]
+  node_desired_size    = 2
+  node_min_size        = 1
+  node_max_size        = 3
 }
 
 ##############
